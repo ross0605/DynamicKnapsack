845389a716216a8b5f2c783eaedb46bfa8268803
ross0605 <ross0605@live.co.uk>
2020-04-07 22:09:11 +0100

    Genetic Algorithm Final Commit

---
+++ GeneticAlgorithm/src/Pair.java
@@ -0,0 +1,31 @@
+
+// Pair class
+class Pair<U, V>
+{
+	public final U first;   	// first field of a Pair
+	public final V second;  	// second field of a Pair
+
+	// Constructs a new Pair with specified values
+	private Pair(U first, V second)
+	{
+		this.first = first;
+		this.second = second;
+	}
+
+	// Factory method for creating a Typed Pair immutable instance
+	public static <U, V> Pair <U, V> of(U a, V b)
+	{
+		// calls private constructor
+		return new Pair<>(a, b);
+	}
+
+	@Override
+	public String toString()
+	{
+		return "(" + first + ", " + second + ")";
+	}
+
+	public U getKey() { return first; }
+	public V getValue() { return second; }
+
+}
